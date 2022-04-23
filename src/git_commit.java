56ca149d7b3ea872d01073c642e7e7e2242eb27e
mooduck27 <maxxx00528@mail.ru>
2022-04-23 13:29:24 +0300

    first commit

---
+++ src/git_commit.java
@@ -0,0 +1,10 @@
+
+
+
+public class git_commit {
+    public static void main(String[] args) {
+        System.out.println("They see me rolling");
+        System.out.println("They hating");
+        System.out.println("a dal' she text ya ne pomnu...");
+    }
+}
